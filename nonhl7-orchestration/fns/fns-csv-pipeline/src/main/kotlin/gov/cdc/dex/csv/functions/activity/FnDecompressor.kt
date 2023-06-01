546a418acb31d0d8de0ab34ad4b9aa00f1254fbd
Peterson <jacob.r.peterson@accenturefederal.com>
2023-06-01 11:56:02 -0500

    added decompressor into new functionality

---
+++ nonhl7-orchestration/fns/fns-csv-pipeline/src/main/kotlin/gov/cdc/dex/csv/functions/activity/FnDecompressor.kt
@@ -0,0 +1,132 @@
+package gov.cdc.dex.csv.functions.activity
+
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.durabletask.azurefunctions.DurableActivityTrigger;
+import com.azure.storage.blob.BlobClientBuilder
+
+import gov.cdc.dex.csv.dtos.ActivityInput
+import gov.cdc.dex.csv.dtos.ActivityOutput
+import gov.cdc.dex.csv.dtos.ActivityParams
+
+import java.io.BufferedOutputStream
+import java.io.InputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.zip.ZipInputStream
+import java.util.logging.Level
+
+class FnDecompressor {
+    private val BLOB_CONNECTION_PARAM = "BlobConnection"
+    private val ZIP_TYPES = listOf("application/zip","application/x-zip-compressed")
+    private val BUFFER_SIZE = 4096
+
+    @FunctionName("DexCsvDecompressor")
+    fun defaultError(
+        @DurableActivityTrigger(name = "input") input: ActivityInput, 
+        context: ExecutionContext 
+    ):ActivityOutput{
+        context.getLogger().info("Running decompressor for input $input");
+        val sourceUrl = input.common.params.originalFileUrl
+        if(sourceUrl == null){
+            return ActivityOutput(errorMessage = "No source URL provided!")
+        }
+        val blobConnection = System.getenv(BLOB_CONNECTION_PARAM)
+        val sourceBlob = BlobClientBuilder().connectionString(blobConnection).endpoint(sourceUrl).buildClient()
+
+        //check for file existence
+        if(!sourceBlob.exists()){
+            return ActivityOutput(errorMessage = "File missing in Azure! $sourceUrl")
+        }
+
+        val contentType = sourceBlob.properties.contentType
+
+        //IF blocks are expressions, and variables can be returned out of them
+        var writtenUrls:List<String> = if(ZIP_TYPES.contains(contentType)){
+            //if ZIP, then unzip and capture the files that were contained
+
+            //stream from the existing zip, and immediately stream the unzipped data back to a new file in processed
+            var downloadStream = sourceBlob.openInputStream()
+            var uploadStreamSupplier = {destinationUrl:String ->
+                val destinationBlob = BlobClientBuilder().connectionString(blobConnection).endpoint(destinationUrl).buildClient()
+                destinationBlob.blockBlobClient.getBlobOutputStream()
+            }
+
+            //TRY blocks are expressions, and variables can be returned out of them
+            var writtenPathsZip:List<String> = try{
+                decompressFileStream(downloadStream, uploadStreamSupplier, "${sourceUrl}-decompressed/")
+            }catch(e:IOException){
+                context.logger.log(Level.SEVERE, "Error unzipping: $sourceUrl", e)
+                return ActivityOutput(errorMessage = "Error unzipping: $sourceUrl : ${e.localizedMessage}")
+            }
+
+            if(writtenPathsZip.isEmpty()){
+                //fail if zip file was empty
+                return ActivityOutput(input.common.params, "Zipped file was empty: $sourceUrl")
+            }
+
+            //return the written paths to the IF statement, to be assigned to variable there
+            writtenPathsZip
+        } else {
+            //if not a zip, pass along the pipeline
+            //wrap the path in a LIST and return to the IF statement
+            listOf(sourceUrl)
+        }
+
+        val fanOutParams = mutableListOf<ActivityParams>()
+        for(url in writtenUrls){
+            val fanParam = input.common.params.copy()
+            fanParam.currentFileUrl = url
+            fanOutParams.add(fanParam)
+        }
+        return ActivityOutput(fanOutParams = fanOutParams)
+    }
+    
+
+    private fun decompressFileStream(inputStream:InputStream, outputStreamSupplier: (String) -> OutputStream, outputUrl:String):List<String>{
+        var writtenPaths : MutableList<String> = mutableListOf();
+
+        //use is equivalent of try-with-resources, will close the input stream at the end
+        inputStream.use{ str ->
+            decompressFileStreamRecursive(str, outputStreamSupplier, outputUrl, writtenPaths)
+        }
+        return writtenPaths
+    }
+    
+
+    private fun decompressFileStreamRecursive(inputStream:InputStream, outputStreamSupplier: (String) -> OutputStream, outputUrl:String, writtenPaths : MutableList<String>){
+        //don't close input stream here because of recursion
+        var zipInputStream = ZipInputStream(inputStream)
+        var zipEntry = zipInputStream.nextEntry;
+        
+        while (zipEntry != null) {
+            //ignore any entries that are a directory. The directory path will be part of the zipEntry.name, and Azure automatically creates the needed directories
+            if (!zipEntry.isDirectory()) {
+                if(zipEntry.name.endsWith(".zip")){
+                    //if a nested zip, recurse
+                    var localPath = zipEntry.name.replace(".zip","/")
+                    decompressFileStreamRecursive(zipInputStream, outputStreamSupplier, outputUrl+localPath, writtenPaths)
+                }else{
+                    // write file content directly to a new blob
+                    var urlToWrite = outputUrl+zipEntry.name
+
+                    var outputStream = outputStreamSupplier.invoke(urlToWrite)
+
+                    //use is equivalent of try-with-resources, will close the output stream when done
+                    BufferedOutputStream(outputStream).use{bufferedOutputStream ->
+                        val bytesIn = ByteArray(BUFFER_SIZE)
+                        var read: Int
+                        while (zipInputStream.read(bytesIn).also { read = it } != -1) {
+                            bufferedOutputStream.write(bytesIn, 0, read)
+                        }
+                    }
+                    //don't close input stream here because of recursion
+        
+                    writtenPaths.add(urlToWrite)
+                }
+            }
+            zipEntry = zipInputStream.getNextEntry();
+        }
+        //don't close input stream here because of recursion
+    }
+}
